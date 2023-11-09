c5b896abc6ac7c9f64bf6263120f1fecaed50b0a
Supdude28 <Radenlow765@gmail.com>
2023-11-09 15:38:54 +0700

    h

---
+++ app/Models/kelasModels.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class kelasModels extends Model
+{
+    use HasFactory;
+}
